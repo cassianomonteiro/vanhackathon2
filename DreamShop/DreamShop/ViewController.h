8b092eda7c8ccad4711d0f63f9690c996f50dd93
Cassiano Monteiro <cassianomonteiro@gmail.com>
2016-10-21 23:11:57 -0200

    Initial project setup

---
+++ DreamShop/DreamShop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DreamShop
+//
+//  Created by Cassiano Monteiro on 21/10/16.
+//  Copyright © 2016 Cassiano Monteiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DreamShop/DreamShopTests/DreamShopTests.m
@@ -0,0 +1,39 @@
+//
+//  DreamShopTests.m
+//  DreamShopTests
+//
+//  Created by Cassiano Monteiro on 21/10/16.
+//  Copyright © 2016 Cassiano Monteiro. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DreamShopTests : XCTestCase
+
+@end
+
+@implementation DreamShopTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
