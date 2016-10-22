3503077bd5686dedbd9d14eeec27668fb4c0a511
Cassiano Monteiro <cassianomonteiro@gmail.com>
2016-10-22 17:02:18 -0200

    Sign-in screen

--- DreamShop/DreamShop/View/NewsFeedVC.m
+++ DreamShop/DreamShop/View/NewsFeedVC.m
@@ -12,6 +12,7 @@
 #import <FirebaseAuth/FirebaseAuth.h>
 #import <FirebaseAuthUI/FirebaseAuthUI.h>
 #import <FirebaseGoogleAuthUI/FirebaseGoogleAuthUI.h>
+#import <FirebaseFacebookAuthUI/FirebaseFacebookAuthUI.h>
 #import <FirebaseTwitterAuthUI/FirebaseTwitterAuthUI.h>
 #import "AlertControllerFactory.h"
 
@@ -31,9 +32,15 @@
     
     self.auth = [FIRAuth auth];
     self.authUI = [FIRAuthUI defaultAuthUI];
-    self.authUI.providers = @[[[FIRGoogleAuthUI alloc] init],
+    self.authUI.providers = @[[[FIRFacebookAuthUI alloc] init],
+                              [[FIRGoogleAuthUI alloc] init],
                               [[FIRTwitterAuthUI alloc] init]];
     self.authUI.delegate = self;
+    
+    if (!self.auth.currentUser) {
+        [self showLoginScreeAnimated:NO];
+    }
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -67,11 +74,22 @@
 
 #pragma mark - Actions
 
+- (void)showLoginScreeAnimated:(BOOL)animated
+{
+    UINavigationController *controller = [self.authUI authViewController];
+    
+    // Remove cancel button
+    controller.topViewController.navigationItem.leftBarButtonItem = nil;
+    
+    [self addSignInLabelToController:controller.topViewController];
+    [self addBackgroundImageToController:controller.topViewController];
+    [self presentViewController:controller animated:animated completion:nil];
+}
+
 - (IBAction)loginTapped:(UIBarButtonItem *)sender
 {
     if (!self.auth.currentUser) {
-        UIViewController *controller = [self.authUI authViewController];
-        [self presentViewController:controller animated:YES completion:nil];
+        [self showLoginScreeAnimated:YES];
     } else {
         [self signOut];
     }
@@ -102,9 +120,97 @@
         
         // sign out from all providers (wipes provider tokens too)
         for (id<FIRAuthProviderUI> provider in self.authUI.providers) {
-            [provider signOut];
+            
+            // Surround with try/catch because not all providers are implemented
+            @try {
+                [provider signOut];
+            } @catch (NSException *exception) {
+                // Nothing to do
+            }
         }
+        
+        [self showLoginScreeAnimated:YES];
     }
+}
+
+#pragma mark - Helpers
+
+- (void)addSignInLabelToController:(UIViewController *)controller
+{
+    UIView *buttonsContainerView = controller.view.subviews.firstObject;
+    
+    UILabel *signInLabel = [[UILabel alloc] init];
+    signInLabel.text = @"Sign in";
+    signInLabel.textColor = [UIColor whiteColor];
+    signInLabel.font = [UIFont systemFontOfSize:44.f];
+    signInLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [controller.view addSubview:signInLabel];
+    
+    NSLayoutConstraint *centerHorizontally = [NSLayoutConstraint constraintWithItem:signInLabel
+                                                                          attribute:NSLayoutAttributeCenterX
+                                                                          relatedBy:NSLayoutRelationEqual
+                                                                             toItem:controller.view
+                                                                          attribute:NSLayoutAttributeCenterX
+                                                                         multiplier:1.0
+                                                                           constant:0.0];
+    
+    NSLayoutConstraint *bottomMargin = [NSLayoutConstraint constraintWithItem:signInLabel
+                                                                    attribute:NSLayoutAttributeBottomMargin
+                                                                    relatedBy:NSLayoutRelationEqual
+                                                                       toItem:buttonsContainerView
+                                                                    attribute:NSLayoutAttributeTop
+                                                                   multiplier:1.0
+                                                                     constant:-20.0];
+    
+    
+    [controller.view addConstraint:centerHorizontally];
+    [controller.view addConstraint:bottomMargin];
+}
+
+- (void)addBackgroundImageToController:(UIViewController *)controller
+{
+    UIImage *image = [UIImage imageNamed:@"Home"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    imageView.translatesAutoresizingMaskIntoConstraints = NO;
+    [controller.view insertSubview:imageView atIndex:0];
+    
+    NSLayoutConstraint *equalWidths = [NSLayoutConstraint constraintWithItem:imageView
+                                                                   attribute:NSLayoutAttributeWidth
+                                                                   relatedBy:NSLayoutRelationEqual
+                                                                      toItem:controller.view
+                                                                   attribute:NSLayoutAttributeWidth
+                                                                  multiplier:1.0
+                                                                    constant:0.0];
+    
+    NSLayoutConstraint *centerHorizontally = [NSLayoutConstraint constraintWithItem:imageView
+                                                                          attribute:NSLayoutAttributeCenterX
+                                                                          relatedBy:NSLayoutRelationEqual
+                                                                             toItem:controller.view
+                                                                          attribute:NSLayoutAttributeCenterX
+                                                                         multiplier:1.0
+                                                                           constant:0.0];
+    
+    NSLayoutConstraint *topMargin = [NSLayoutConstraint constraintWithItem:imageView
+                                                                 attribute:NSLayoutAttributeTop
+                                                                 relatedBy:NSLayoutRelationEqual
+                                                                    toItem:controller.view
+                                                                 attribute:NSLayoutAttributeTop
+                                                                multiplier:1.0
+                                                                  constant:0.0];
+    
+    NSLayoutConstraint *heightRatio = [NSLayoutConstraint constraintWithItem:imageView
+                                                                   attribute:NSLayoutAttributeHeight
+                                                                   relatedBy:NSLayoutRelationEqual
+                                                                      toItem:imageView
+                                                                   attribute:NSLayoutAttributeWidth
+                                                                  multiplier:(image.size.height/image.size.width)
+                                                                    constant:0.0];
+    
+    
+    [controller.view addConstraint:equalWidths];
+    [controller.view addConstraint:centerHorizontally];
+    [controller.view addConstraint:topMargin];
+    [controller.view addConstraint:heightRatio];
 }
 
 @end
